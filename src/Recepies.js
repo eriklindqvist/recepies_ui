9a1408b6f472ac0ee719e5cab8dfcce394364914
Erik Lindqvist <erikjo82+github@gmail.com>
2017-12-27 09:20:29 +0100

    Error handling to fetch names call

--- src/Recepies.js
+++ src/Recepies.js
@@ -5,12 +5,24 @@ class Recepies extends Component {
   constructor(props) {
     super(props);
     this.state = { recepies: [], loading: true };
+
+    this.checkStatus = (response) => {
+      if (response.status >= 200 && response.status < 300) {
+        return response;
+      } else {
+        let error = new Error(response.statusText);
+        error.response = response;
+        throw error;
+      }
+    }
   }
 
   componentDidMount() {
-    return fetch("http://localhost:3000/recepies/names")
+    return fetch("/api/recepies/names")
+     .then(this.checkStatus)
      .then(response => response.json())
-     .then((json) => this.setState({recepies: json, loading: false}));
+     .then(json => this.setState({recepies: json, loading: false}))
+     .catch(e => console.log(e));
    }
 
   render() {
